56a0465b771d3654b915f895e8e565c35e80c93b
Paulo Santos <paulomartins_santos@hotmail.com>
2022-08-15 19:15:31 -0300

    update

---
+++ robot/Aula01.py
@@ -0,0 +1,29 @@
+""" nome  =  input('Digite seu nome:  ')
+idade =  input('Digite sua idade: ') 
+peso  =  input('Digite seu peso:  ')
+print ('Nome: '+ nome,'Idade: ' + idade, 'Peso: ' + peso) """
+
+
+## Desafio 01: Crie um script Python que leio o nome de uma pessoa e mostre uma mensagem de boas-vindas com o valor digitado
+
+""" nome = input('Qual é o seu nome?')
+print ('Olá '+ nome + '! Prazer em te conhecer!') """
+
+## Desafio 02: Crie um script Python que leia o dia, o mês e o ano de uma pessoa e mostre uma mensagem com a data formatada
+##Ex:
+##Dia: 04
+##Mês: Jan
+##Ano: 1986
+## Você nasceu no dia 04 de Jan de 1986. Correto? 
+
+
+## Desafio 03: Crie um script Python que leia dois números e mostre a soma entre eles
+
+num1 = input('Digite o primeiro valor: ')
+num2 = input('Digite o segundo valor:  ')
+soma = num1 + num2
+
+print('O resultado da soma é: ' + soma)
+
+
+
